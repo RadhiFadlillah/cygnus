9580813bebe2a96aad62b9c2a0e533fbd18ee59f
Radhi Fadlillah <m.radhi.f@gmail.com>
2019-03-13 08:38:02 +0700

    Put live video inside div container

--- view/js/page/live.js
+++ view/js/page/live.js
@@ -1,15 +1,17 @@
 var template = `
 <div id="page-live">
     <h1 class="page-header">
-        Cashier #1
+        Live Stream
     </h1>
-    <video id="live-viewer" class="cygnus-video video-js" controls preload="auto">
-        <source src="/playlist/live" type="application/vnd.apple.mpegurl">
-        <p class="vjs-no-js">
-            To view this video please enable JavaScript, and consider upgrading to a web browser that
-            <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
-        </p>
-    </video>
+    <div class="video-container">
+        <video id="live-viewer" class="cygnus-video video-js" controls preload="auto">
+            <source src="/playlist/live" type="application/vnd.apple.mpegurl">
+            <p class="vjs-no-js">
+                To view this video please enable JavaScript, and consider upgrading to a web browser that
+                <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
+            </p>
+        </video>
+    </div>
 </div>`;
 
 export default {
