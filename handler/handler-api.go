ec5626a7a41dc0672ba8fa4ea38de6ca3669cf61
Radhi Fadlillah <m.radhi.f@gmail.com>
2019-03-27 22:28:10 +0700

    If no account exist in database, use default account

--- handler/handler-api.go
+++ handler/handler-api.go
@@ -23,6 +23,51 @@ func (h *WebHandler) APILogin(w http.ResponseWriter, r *http.Request, ps httprou
 	err := json.NewDecoder(r.Body).Decode(&request)
 	checkError(err)
 
+	// Prepare function to generate session
+	genSession := func(expTime time.Duration) {
+		// Create session ID
+		sessionID, err := uuid.NewV4()
+		checkError(err)
+
+		// Save session ID to cache
+		strSessionID := sessionID.String()
+		h.SessionCache.Set(strSessionID, request.Username, expTime)
+
+		// Save user's session IDs to cache as well
+		// useful for mass logout
+		sessionIDs := []string{strSessionID}
+		if val, found := h.UserCache.Get(request.Username); found {
+			sessionIDs = val.([]string)
+			sessionIDs = append(sessionIDs, strSessionID)
+		}
+		h.UserCache.Set(request.Username, sessionIDs, -1)
+
+		// Return session ID to user in cookies
+		http.SetCookie(w, &http.Cookie{
+			Name:    "session-id",
+			Value:   strSessionID,
+			Path:    "/",
+			Expires: time.Now().Add(expTime),
+		})
+
+		w.Header().Set("Content-Type", "text/plain")
+		fmt.Fprint(w, strSessionID)
+	}
+
+	// Check if user's database is empty
+	dbIsEmpty := false
+	h.DB.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte("user"))
+		dbIsEmpty = bucket == nil || bucket.Stats().KeyN == 0
+		return nil
+	})
+
+	// If database still empty, and user uses default account, let him in
+	if dbIsEmpty && request.Username == "admin" && request.Password == "admin" {
+		genSession(time.Hour)
+		return
+	}
+
 	// Get account data from database
 	var hashedPassword []byte
 	err = h.DB.View(func(tx *bolt.Tx) error {
@@ -54,33 +99,8 @@ func (h *WebHandler) APILogin(w http.ResponseWriter, r *http.Request, ps httprou
 		expTime = -1
 	}
 
-	// Create session ID
-	sessionID, err := uuid.NewV4()
-	checkError(err)
-
-	// Save session ID to cache
-	strSessionID := sessionID.String()
-	h.SessionCache.Set(strSessionID, request.Username, expTime)
-
-	// Save user's session IDs to cache as well
-	// useful for mass logout
-	sessionIDs := []string{strSessionID}
-	if val, found := h.UserCache.Get(request.Username); found {
-		sessionIDs = val.([]string)
-		sessionIDs = append(sessionIDs, strSessionID)
-	}
-	h.UserCache.Set(request.Username, sessionIDs, -1)
-
-	// Return session ID to user in cookies
-	http.SetCookie(w, &http.Cookie{
-		Name:    "session-id",
-		Value:   strSessionID,
-		Path:    "/",
-		Expires: time.Now().Add(expTime),
-	})
-
-	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprint(w, strSessionID)
+	// Create session
+	genSession(expTime)
 }
 
 // APILogout is handler for POST /api/logout
